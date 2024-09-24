60a537718ebf4c0cb6ce245237bd7ac6777e5656
SteelTitansax <manuel.portero.leiva@gmail.com>
2024-09-24 12:30:59 +0200

    design screen2

--- tankdesigner/tankdesigner/urls.py
+++ tankdesigner/tankdesigner/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from tankdesignersimulator import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.design, name='design'),
 ]
